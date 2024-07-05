c917a9d4c2b75701e746233472c547ea6109f011
martian0x80 <26498920+martian0x80@users.noreply.github.com>
2024-07-05 21:31:20 +0530

    feat: more dots!

---
+++ .config/hypr/UserScripts/RofiBeats.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */  ##
+# For Rofi Beats to play online Music or Locally save media files
+
+# Directory local music folder
+mDIR="$HOME/Music/"
+
+# Directory for icons
+iDIR="$HOME/.config/swaync/icons"
+
+# Online Stations. Edit as required
+declare -A online_music=(
+  ["AfroBeatz 2024 🎧"]="https://www.youtube.com/watch?v=7uB-Eh9XVZQ"
+  ["Lofi Girl ☕️🎶"]="https://play.streamafrica.net/lofiradio"
+  ["Easy Rock 96.3 FM 📻🎶"]="https://radio-stations-philippines.com/easy-rock"
+  ["Wish 107.5 FM 📻🎶"]="https://radio-stations-philippines.com/dwnu-1075-wish"
+  ["Wish 107.5 YT Pinoy HipHop 🎻🎶"]="https://youtube.com/playlist?list=PLkrzfEDjeYJnmgMYwCKid4XIFqUKBVWEs&si=vahW_noh4UDJ5d37"
+  ["Top Youtube Music 2023 ☕️🎶"]="https://youtube.com/playlist?list=PLDIoUOhQQPlXr63I_vwF9GD8sAKh77dWU&si=y7qNeEVFNgA-XxKy"
+  ["Wish 107.5 YT Wishclusives ☕️🎶"]="https://youtube.com/playlist?list=PLkrzfEDjeYJn5B22H9HOWP3Kxxs-DkPSM&si=d_Ld2OKhGvpH48WO"
+  ["Chillhop ☕️🎶"]="http://stream.zeno.fm/fyn8eh3h5f8uv"
+  ["SmoothChill ☕️🎶"]="https://media-ssl.musicradio.com/SmoothChill"
+  ["Relaxing Music ☕️🎶"]="https://youtube.com/playlist?list=PLMIbmfP_9vb8BCxRoraJpoo4q1yMFg4CE"
+  ["Youtube Remix 📻🎶"]="https://youtube.com/playlist?list=PLeqTkIUlrZXlSNn3tcXAa-zbo95j0iN-0"
+  ["Korean Drama OST 📻🎶"]="https://youtube.com/playlist?list=PLUge_o9AIFp4HuA-A3e3ZqENh63LuRRlQ"
+)
+
+# Populate local_music array with files from music directory and subdirectories
+populate_local_music() {
+  local_music=()
+  filenames=()
+  while IFS= read -r file; do
+    local_music+=("$file")
+    filenames+=("$(basename "$file")")
+  done < <(find "$mDIR" -type f \( -iname "*.mp3" -o -iname "*.flac" -o -iname "*.wav" -o -iname "*.ogg" -o -iname "*.mp4" \))
+}
+
+# Function for displaying notifications
+notification() {
+  notify-send -u normal -i "$iDIR/music.png" "Playing: $@"
+}
+
+# Main function for playing local music
+play_local_music() {
+  populate_local_music
+
+  # Prompt the user to select a song
+  choice=$(printf "%s\n" "${filenames[@]}" | rofi -i -dmenu -config ~/.config/rofi/config-rofi-Beats.rasi -p "Local Music")
+
+  if [ -z "$choice" ]; then
+    exit 1
+  fi
+
+  # Find the corresponding file path based on user's choice and set that to play the song then continue on the list
+  for (( i=0; i<"${#filenames[@]}"; ++i )); do
+    if [ "${filenames[$i]}" = "$choice" ]; then
+		
+	    notification "$choice"
+
+      # For some reason wont start playlist at 0
+      if [[ $i -eq 0 ]]; then
+        # Play the selected local music file using mpv
+        mpv --loop-playlist --vid=no "$mDIR/$choice" 
+      	
+      else
+        file=$i
+        # Play the selected local music file using mpv
+        mpv --playlist-start="$file" --loop-playlist --vid=no "$mDIR/$choice"
+      fi
+      break
+    fi
+  done
+}
+
+# Main function for shuffling local music
+shuffle_local_music() {
+  notification "Shuffle local music"
+
+  # Play music in $mDIR on shuffle
+  mpv --shuffle --loop-playlist --vid=no "$mDIR"
+}
+
+# Main function for playing online music
+play_online_music() {
+  choice=$(printf "%s\n" "${!online_music[@]}" | rofi -i -dmenu -config ~/.config/rofi/config-rofi-Beats.rasi -p "Online Music")
+
+  if [ -z "$choice" ]; then
+    exit 1
+  fi
+
+  link="${online_music[$choice]}"
+
+  notification "$choice"
+  
+  # Play the selected online music using mpv
+  mpv --shuffle --vid=no "$link"
+}
+
+# Check if an online music process is running and send a notification, otherwise run the main function
+pkill mpv && notify-send -u low -i "$iDIR/music.png" "Music stopped" || {
+
+# Prompt the user to choose between local and online music
+user_choice=$(printf "Play from Online Stations\nPlay from Music Folder\nShuffle Play from Music Folder" | rofi -dmenu -config ~/.config/rofi/config-rofi-Beats-menu.rasi -p "Select music source")
+
+  case "$user_choice" in
+    "Play from Music Folder")
+      play_local_music
+      ;;
+    "Play from Online Stations")
+      play_online_music
+      ;;
+    "Shuffle Play from Music Folder")
+      shuffle_local_music
+      ;;
+    *)
+      echo "Invalid choice"
+      ;;
+  esac
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# My Fedora Workstation 38 Dotfiles
+
+Welcome to my Fedora Workstation 38 dotfiles repository.
+The repository content is too old. I have since moved from Gnome/KDE Plasma to hyprland.
+
+### Here's a peek of my current desktop:
+![Current Desktop](https://raw.githubusercontent.com/martian0x80/dotfiles/master/Pictures/hyprland_rice1.png)
+![Current Desktop](https://raw.githubusercontent.com/martian0x80/dotfiles/master/Pictures/hyprland_rice0.png)
+
+### And here's the old KDE Plasma desktop:
+![KDE Desktop](https://raw.githubusercontent.com/martian0x80/dotfiles/master/Pictures/Screenshot_20230804_161433-1.png "Take a peek")
+![KDE Desktop](https://raw.githubusercontent.com/martian0x80/dotfiles/master/Pictures/Screenshot_20230830_121849-obfuscated.png "Take a peek")
+
+## What's Inside
+
+- **Desktop Environment**: KDE Plasma
+- **Display Server**: Wayland
+- **Global Theme**: Glassy
+- **Plasma Style**: WinSur-dark.
+- **Color Schemes**: Choose from "Materia Dark", "Night Owl" or "Glorious Dark" (\* preferred).
+- **Window Decorations**: Klassy with Redmond icons.
+- **Icons**: La Capitaine
+- **Splash Screen**: [BeautifulTreeAnimation](https://store.kde.org/p/1433200)
+- **Plymouth Theme**: Explore the captivating "Lone" theme and more [here](https://github.com/adi1090x/plymouth-themes).
+- **Terminal**: Kitty, Konsole.
+- **Dock**: Latte Dock.
+- **Editor**: Helix, NeoVim, Kate.
+
+## Make It Your Own
+
+Feel free to explore and adapt these dotfiles to suit your preferences. Please, do not just smash the configs into `~/.config`. Just follow these steps:
+
+1. Clone the repository.
+2. Customize the configuration files.
+3. Copy or replace these with the defaults in ```$HOME/.config/```
